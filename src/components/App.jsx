6adc0f3c86e6844652e225c0830b9d0b38918246
wwwchendev <wwwchendev@gmail.com>
2024-02-07 00:03:13 +0800

    建立全域樣式 createGlobalStyle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import { Button } from '@/components/common';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body{
+  background: white;
+  min-height: 100vh;
+  margin: 0;
+  color: black;
+  font-family: 'Protest Riot', sans-serif;
+}
+`;
 
 function App() {
   return (
     <>
-      <h1>APP</h1>
+      <GlobalStyle />
+      <h1>Hello Styled Components!</h1>
       <Button>測試</Button>
       <Button disabled>測試</Button>
     </>
