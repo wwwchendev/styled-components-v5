f60d50cc4f7767b2fa97afde7d49254803c20164
wwwchendev <wwwchendev@gmail.com>
2024-02-06 23:40:57 +0800

    元件載入方式+元件撰寫方法

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from '@/components/common';
+
 function App() {
   return (
     <>
       <h1>APP</h1>
+      <Button>測試</Button>
+      <Button disabled>測試</Button>
     </>
   );
 }
