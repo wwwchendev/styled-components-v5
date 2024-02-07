614594d7020aedec77a353bddf156f72c7071feb
wwwchendev <wwwchendev@gmail.com>
2024-02-08 01:39:18 +0800

    簡化樣式化元件的內部代碼: 將樣式邏輯分離到單獨函數中

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,13 +1,30 @@
 import styled, { css } from 'styled-components';
 
+// 簡化樣式化元件的內部代碼: 將樣式邏輯分離到單獨函數中
+const largeStyles = ({ large }) => {
+  if (large) {
+    return css`
+      padding: 10px;
+      border-radius: 5px;
+      font-size: 1.5em;
+    `;
+  } else {
+    return css`
+      padding: 8px;
+      border-radius: 4px;
+      font-size: 1em;
+    `;
+  }
+};
+
 export const Button = styled.button`
   color: white;
   background: ${props =>
     props.$secondary ? props.theme.secondaryColor : props.theme.primaryColor};
   font-weight: bold;
   /* padding: ${props => (props.large ? '16px' : '8px')}; */
-  ${p =>
-    p.$large
+  /* ${props =>
+    props.$large
       ? css`
           padding: 10px;
           border-radius: 5px;
@@ -17,7 +34,8 @@ export const Button = styled.button`
           padding: 8px;
           border-radius: 4px;
           font-size: 1em;
-        `}
+        `} */
+  ${largeStyles}
   box-shadow: none;
   border: none;
   width: 100%;
